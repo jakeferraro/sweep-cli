7fab93faaff9ed402c50a0eee6251ca894b29786
Jake Ferraro <jakeferraro@proton.me>
2025-12-01 20:39:09 -0800

    setup script

---
+++ setup.py
@@ -0,0 +1,31 @@
+"""Setup script for sweep CLI."""
+
+from setuptools import setup, find_packages
+
+setup(
+    name='sweep-cli',
+    version='1.0.0',
+    description='Filesystem analyzer with Finder tag integration',
+    author='Jake Ferraro',
+    url='https://github.com/jakeferraro/sweep-cli',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'sweep=sweep:main',
+        ],
+    },
+    python_requires='>=3.9',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: MacOS',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Topic :: System :: Filesystems',
+        'Topic :: Utilities',
+    ],
+)
